d82e741183aba1eae441f99ed09561b20559a4bb
HoChunChit(24202495) <135957466+Tony0824X@users.noreply.github.com>
2025-12-01 14:58:01 +0800

    Initial Commit

---
+++ SpatialVoice/SpatialVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialVoiceApp.swift
+//  SpatialVoice
+//
+//  Created by 1234 on 1/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpatialVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
